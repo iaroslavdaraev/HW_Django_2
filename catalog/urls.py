ec13283db313e21d51dbc57b70b2a57aaac8066b
Iaroslav <yaroslav.da@mail.ru>
2024-03-24 00:01:35 +0300

    add urlpatterns

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from catalog.views import index_homepage, contacts
@@ -5,4 +7,4 @@ from catalog.views import index_homepage, contacts
 urlpatterns = [
     path('', index_homepage),
     path('contacts/', contacts)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
