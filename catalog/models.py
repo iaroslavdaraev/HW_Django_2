ef36129b61698f359f1643ded849ff26acd327c0
Iaroslav <yaroslav.da@mail.ru>
2024-03-23 23:24:21 +0300

    create models product and category with str and meta

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    pass
+
+    def __str__(self):
+        pass
+
+    class Meta:
+        pass
+
+
+class Category(models.Model):
+    pass
+
+    def __str__(self):
+        pass
+
+    class Meta:
+        pass
